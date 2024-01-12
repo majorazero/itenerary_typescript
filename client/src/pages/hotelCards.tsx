88b054beb812a68c02a87f7a110c6c010e4d66c0
Dan Hsu <daniel.cw.hsu@gmail.com>
2024-01-11 17:37:38 -0800

    added alt tags

--- client/src/pages/hotelCards.tsx
+++ client/src/pages/hotelCards.tsx
@@ -11,7 +11,7 @@ const hotelCard: FunctionComponent<any> = (hotel, setHotel) => (
         <img className="hotel-pic mx-auto" src={hotel.image_url} alt={`Location at ${hotel.name}`}></img>
         <h5 className="hotel-name">{hotel.name}</h5>
         <h6 className="hotel-price">{hotel.price}</h6>
-        <img className="py-2 hotel-star" src={yelpStar(hotel.rating)}></img>
+        <img className="py-2 hotel-star" src={yelpStar(hotel.rating)} alt={`${hotel.rating} stars`}></img>
         <small>Reviews: {hotel.review_count}</small>
         <h6>Phone: {hotel.phone}</h6>
         {hotel.location.display_address.map((line: string) => 
@@ -27,7 +27,7 @@ const hotelCards: FunctionComponent<HotelCardsProps> = ({ options }) => {
             <div className="container card py-5">
                 <div className="row">
                     <div className='col-12'>
-                        <img className="error-img" src="img/oops.png" />
+                        <img className="error-img" src="img/oops.png" alt="man lost on top of mountain" />
                         <div>
                             Seems like we can't find any hotels at the location! Try again!
                         </div>
