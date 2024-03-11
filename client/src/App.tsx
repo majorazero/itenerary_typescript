6e1668cc87de7c9db1aad4bbc9a6cc73e0d8e04e
Dan Hsu <daniel.cw.hsu@gmail.com>
2024-03-11 15:17:26 -0700

    added sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import './css/App.css';
+import './css/App.scss';
 
 import Search from "./pages/search";
 import HotelCards from "./pages/hotelCards";
